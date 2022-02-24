597665a0adde039279c3d149f752127b00e7b472
Tony Cai <cailijian1991@gmail.com>
2022-02-24 22:39:38 +1030

    style Card component

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,5 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.a`
+  display: flex;
+  box-sizing: border-box;
+  padding: 5px;
+  padding-left: 10px;
+  margin: 0px 10px;
+  margin-bottom: 5px;
+  min-height: 20px;
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 #091e4240;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f4f4f4;
+  }
+`;
 
 export default function Card({ title }) {
-  return <div>{title}</div>;
+  return <Container>{title}</Container>;
 }
